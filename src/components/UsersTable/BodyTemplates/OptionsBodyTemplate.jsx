806835a192424b10b05fba3a300a015fcdc3a49f
David Cardenas <davidcardenasgonz@gmail.com>
2024-12-23 18:21:30 -0600

    refactor: update import paths for UserModal and improve code formatting in UsersTable component

--- src/components/UsersTable/BodyTemplates/OptionsBodyTemplate.jsx
+++ src/components/UsersTable/BodyTemplates/OptionsBodyTemplate.jsx
@@ -2,70 +2,92 @@ import { useState } from "react";
 import { deleteUser, updateUser } from "../../../api/users";
 import { useRef } from "react";
 
-import UserModal from "../NewUserModal";
 import { Button } from "primereact/button";
 import { Menu } from "primereact/menu";
 import { Dialog } from "primereact/dialog";
-
+import UserModal from "../Modals/NewUserModal";
 
 const handleModify = (id, user) => {
-    updateUser(id, user);
-}
+  updateUser(id, user);
+};
 
 const handleDelete = (id) => {
-    deleteUser(id);
+  deleteUser(id);
 };
 
 export const OptionsBodyTemplate = (rowData) => {
-    const [deleteDialogVisible, setDeleteDialogVisible] = useState(false);
-    const [selectedId, setSelectedId] = useState(null);
-    
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [deleteDialogVisible, setDeleteDialogVisible] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
 
-    const handleModalOpen = () => setIsModalOpen(true);
-    const handleModalClose = () => setIsModalOpen(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const confirmDelete = (id) => {
-        setSelectedId(id);
-        setDeleteDialogVisible(true);
-    };
+  const handleModalOpen = () => setIsModalOpen(true);
+  const handleModalClose = () => setIsModalOpen(false);
 
-    const onDeleteConfirmed = () => {
-        handleDelete(selectedId);
-        setDeleteDialogVisible(false);
-    };
+  const confirmDelete = (id) => {
+    setSelectedId(id);
+    setDeleteDialogVisible(true);
+  };
 
-    const items = [
-        { label: 'Modificar', icon: 'pi pi-pencil', command: () => handleModalOpen()},
-        { label: 'Eliminar', icon: 'pi pi-trash', command: () => confirmDelete(rowData._id) }
-    ];
+  const onDeleteConfirmed = () => {
+    handleDelete(selectedId);
+    setDeleteDialogVisible(false);
+  };
 
-    const menu = useRef(null);
+  const items = [
+    {
+      label: "Modificar",
+      icon: "pi pi-pencil",
+      command: () => handleModalOpen(),
+    },
+    {
+      label: "Eliminar",
+      icon: "pi pi-trash",
+      command: () => confirmDelete(rowData._id),
+    },
+  ];
 
-    return (
-        <>
+  const menu = useRef(null);
+
+  return (
+    <>
+      <Button
+        icon="pi pi-ellipsis-v"
+        text
+        onClick={(e) => menu.current.toggle(e)}
+      >
+        <Menu model={items} popup ref={menu} />
+      </Button>
+      <Dialog
+        header="Confirmar eliminación"
+        visible={deleteDialogVisible}
+        style={{ width: "350px" }}
+        footer={
+          <div>
             <Button
-                icon="pi pi-ellipsis-v"
-                text
-                onClick={(e) => menu.current.toggle(e)}
-            >
-                <Menu model={items} popup ref={menu} />
-            </Button>
-            <Dialog
-                header="Confirmar eliminación"
-                visible={deleteDialogVisible}
-                style={{ width: '350px' }}
-                footer={
-                    <div>
-                        <Button label="No" icon="pi pi-times" onClick={() => setDeleteDialogVisible(false)} className="p-button-text" />
-                        <Button label="Sí" icon="pi pi-check" onClick={onDeleteConfirmed} autoFocus />
-                    </div>
-                }
-                onHide={() => setDeleteDialogVisible(false)}
-            >
-                <p>¿Está seguro de que desea eliminar este proyecto?</p>
-            </Dialog>
-            <UserModal open={isModalOpen} handleClose={handleModalClose} handleSave={handleModify} data={rowData}/>
-        </>
-    );
-}
+              label="No"
+              icon="pi pi-times"
+              onClick={() => setDeleteDialogVisible(false)}
+              className="p-button-text"
+            />
+            <Button
+              label="Sí"
+              icon="pi pi-check"
+              onClick={onDeleteConfirmed}
+              autoFocus
+            />
+          </div>
+        }
+        onHide={() => setDeleteDialogVisible(false)}
+      >
+        <p>¿Está seguro de que desea eliminar este proyecto?</p>
+      </Dialog>
+      <UserModal
+        open={isModalOpen}
+        handleClose={handleModalClose}
+        handleSave={handleModify}
+        data={rowData}
+      />
+    </>
+  );
+};
--- src/components/UsersTable/index.jsx
+++ src/components/UsersTable/index.jsx
@@ -1,100 +1,103 @@
-import { useState } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Box } from '@mui/material';
-import { FilterMatchMode } from 'primereact/api';
-import UserModal from './NewUserModal';
-import TableHeader from './TableHeader';
-import RoleFilterTemplate from './FilterTemplates/RoleFilterTemplate';
-import { OptionsBodyTemplate } from './BodyTemplates/OptionsBodyTemplate';
-import { createUser } from '../../api/users';
-import PropTypes from 'prop-types';
-import { RoleBodyTemplate } from './BodyTemplates/RoleBodyTemplate';
+import { useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Box } from "@mui/material";
+import { FilterMatchMode } from "primereact/api";
+import TableHeader from "./TableHeader";
+import RoleFilterTemplate from "./FilterTemplates/RoleFilterTemplate";
+import { OptionsBodyTemplate } from "./BodyTemplates/OptionsBodyTemplate";
+import { createUser } from "../../api/users";
+import PropTypes from "prop-types";
+import { RoleBodyTemplate } from "./BodyTemplates/RoleBodyTemplate";
+import UserModal from "./Modals/NewUserModal";
 
 const UsersTable = ({ data, loading, error }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const handleModalOpen = () => setIsModalOpen(true);
-    const handleModalClose = () => setIsModalOpen(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleModalOpen = () => setIsModalOpen(true);
+  const handleModalClose = () => setIsModalOpen(false);
 
-    const [filters, setFilters] = useState({
-        name: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        email: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-        role: { value: null, matchMode: FilterMatchMode.EQUALS },
-    });
+  const [filters, setFilters] = useState({
+    name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    email: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    role: { value: null, matchMode: FilterMatchMode.EQUALS },
+  });
 
-    return (
-        <Box
-            sx={{
-                margin: '12vh 2rem 3vh 2rem',
-            }}
-        >
-            {error && <Box sx={{ color: 'red', marginBottom: '1rem' }}>{error}</Box>}
-            <UserModal 
-                handleClose={handleModalClose} 
-                handleSave={createUser} 
-                open={isModalOpen} 
-            />
-            <DataTable
-                emptyMessage="No se encontraron usuarios."
-                filterDisplay='row'
-                filters={filters}
-                header={<TableHeader onOpenModal={handleModalOpen}/>}
-                loading={loading}
-                onFilter={(e) => setFilters(e.filters)}
-                paginator
-                removableSort
-                rows={5}
-                rowsPerPageOptions={[5, 10, 25]}
-                size='small'
-                stripedRows
-                value={data}
-            >
-                <Column 
-                    field='name'
-                    filter 
-                    filterPlaceholder="Buscar por nombre" 
-                    header="Nombre" 
-                    sortable 
-                    style={{ width: '40%' }} 
-                />
-                <Column 
-                    field='email'
-                    filter 
-                    filterPlaceholder="Buscar por correo" 
-                    header="Email" 
-                    sortable 
-                    style={{ width: '30%' }} 
-                />
-                <Column
-                    body={RoleBodyTemplate}
-                    field='role'
-                    filter
-                    filterElement={RoleFilterTemplate}
-                    header="Rol"
-                    showFilterMenu={false}
-                    sortable
-                    style={{ width: '20%' }}
-                />
-                <Column
-                    body={OptionsBodyTemplate}
-                    style={{ width: '5%' }}
-                />
-            </DataTable>
-        </Box>
-    );
-}
+  return (
+    <Box
+      sx={{
+        margin: "12vh 2rem 3vh 2rem",
+      }}
+    >
+      {error && <Box sx={{ color: "red", marginBottom: "1rem" }}>{error}</Box>}
+      <UserModal
+        handleClose={handleModalClose}
+        handleSave={createUser}
+        open={isModalOpen}
+      />
+      <DataTable
+        emptyMessage="No se encontraron usuarios."
+        filterDisplay="row"
+        filters={filters}
+        header={<TableHeader onOpenModal={handleModalOpen} />}
+        loading={loading}
+        onFilter={(e) => setFilters(e.filters)}
+        paginator
+        removableSort
+        rows={5}
+        rowsPerPageOptions={[5, 10, 25]}
+        size="small"
+        stripedRows
+        value={data}
+      >
+        <Column
+          field="name"
+          filter
+          filterPlaceholder="Buscar por nombre"
+          header="Nombre"
+          sortable
+          style={{ width: "40%" }}
+        />
+        <Column
+          field="email"
+          filter
+          filterPlaceholder="Buscar por correo"
+          header="Email"
+          sortable
+          style={{ width: "30%" }}
+        />
+        <Column
+          body={RoleBodyTemplate}
+          field="role"
+          filter
+          filterElement={RoleFilterTemplate}
+          header="Rol"
+          showFilterMenu={false}
+          sortable
+          style={{ width: "20%" }}
+        />
+        <Column body={OptionsBodyTemplate} style={{ width: "5%" }} />
+      </DataTable>
+    </Box>
+  );
+};
 
 UsersTable.propTypes = {
-    data: PropTypes.arrayOf(
-        PropTypes.shape({
-            email: PropTypes.string,
-            name: PropTypes.string,
-            permissions: PropTypes.arrayOf(PropTypes.string),
-            role: PropTypes.oneOf(["jefe_academico", "profesor", "presidente_academia", "coordinador_carrera", "estudiante"]),
-        })
-    ),
-    error: PropTypes.string,
-    loading: PropTypes.bool,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      email: PropTypes.string,
+      name: PropTypes.string,
+      permissions: PropTypes.arrayOf(PropTypes.string),
+      role: PropTypes.oneOf([
+        "jefe_academico",
+        "profesor",
+        "presidente_academia",
+        "coordinador_carrera",
+        "estudiante",
+      ]),
+    })
+  ),
+  error: PropTypes.string,
+  loading: PropTypes.bool,
 };
 
 export default UsersTable;
